e1cc41b5735abc31fa277a99a6853221cea4e73d
Eryczal <79263592+Eryczal@users.noreply.github.com>
2024-09-10 21:52:23 +0200

    Update filtersSlice

--- src/app/filtersSlice.ts
+++ src/app/filtersSlice.ts
@@ -1,12 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Action, Filter } from "./types";
+
+const initialState: Filter = {
+    name: "",
+    username: "",
+    email: "",
+    phone: "",
+};
 
 const filtersSlice = createSlice({
     name: "filter",
-    initialState: "",
+    initialState,
     reducers: {
-        setFilter: (state, action) => action.payload,
+        setFilter: (state: Filter, action: PayloadAction<Action>) => {
+            const { type, text } = action.payload;
+            state[type] = text;
+        },
+        clearFilters: () => {
+            return initialState;
+        },
     },
 });
 
-export const { setFilter } = filtersSlice.actions;
+export const { setFilter, clearFilters } = filtersSlice.actions;
 export default filtersSlice.reducer;
