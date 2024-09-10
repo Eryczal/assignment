4cdffe2322ee7d510aaae7c85438cbf320efa1ae
Eryczal <79263592+Eryczal@users.noreply.github.com>
2024-09-10 21:54:27 +0200

    Display users in table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,9 +1,65 @@
+import "./Table.css";
 import { useGetAllUsersQuery } from "../app/userApi";
+import { useSelector } from "react-redux";
+import { selectFilteredUsers } from "../app/selectors";
+import { User } from "../app/types";
 
 const Table = () => {
-    const { data, error, isLoading } = useGetAllUsersQuery();
+    const { error, isLoading } = useGetAllUsersQuery();
+    const filteredUsers = useSelector(selectFilteredUsers);
 
-    return <></>;
+    if (isLoading) {
+        return <div>Loading users</div>;
+    }
+
+    if (error) {
+        return <div>Error while loading users</div>;
+    }
+
+    if (filteredUsers) {
+        return (
+            <div className="table-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>
+                                <p>Name</p>
+                            </th>
+                            <th>
+                                <p>Username</p>
+                            </th>
+                            <th>
+                                <p>Email</p>
+                            </th>
+                            <th>
+                                <p>Phone</p>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {filteredUsers.length === 0 ? (
+                            <tr>
+                                <td colSpan={4} className="no-users">
+                                    No users found
+                                </td>
+                            </tr>
+                        ) : (
+                            filteredUsers.map((user: User) => {
+                                return (
+                                    <tr key={user.id}>
+                                        <td>{user.name}</td>
+                                        <td>{user.username}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.phone}</td>
+                                    </tr>
+                                );
+                            })
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 };
 
 export default Table;
